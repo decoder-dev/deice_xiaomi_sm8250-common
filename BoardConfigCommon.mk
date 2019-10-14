9e87c749400aab7d47c1f30c5e7cd6d63c4852f8
Minchan Kim <minchan@google.com>
2019-10-14 14:22:45 -0700

    sm8250-common: Enable zram writeback
    Bug: 140882488
    Change-Id: I34a029bf1c95c169e732e7269a840abde4ba118e
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,12 +129,11 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/munch
 TARGET_KERNEL_ARCH := arm64
 KERNEL_LD := LD=ld.lld
-BOARD_KERNEL_CMDLINE += kpti=off
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := sdclang
-TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 LLVM_IAS=1
+TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+
 
 
 # Partitions
