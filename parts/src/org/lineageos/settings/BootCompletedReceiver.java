36a56672fab12238a31c9bac3094f9ccf0e9a4b8
TheCrazyLex <alex.naidis@linux.com>
2020-10-29 12:32:39 +0100

    Revert "HDR for dolby vision"
    This reverts commit 67b322a8caddeae2c1b52da989cfcf2c0a0db3be.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,13 +20,12 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
 import android.content.SharedPreferences;
 import android.os.SystemProperties;
 import android.util.Log;
 import androidx.preference.PreferenceManager;
-import android.os.IBinder;
-import android.view.Display.HdrCapabilities;
-import android.view.SurfaceControl;
+
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
@@ -50,14 +49,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         PopupCameraUtils.checkPopupCameraService(context);
         ThermalUtils.startService(context);
         FileUtils.enableService(context);
-        RefreshUtils.startService(context);
-
-        // Override HDR types
-        final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
-        SurfaceControl.overrideHdrTypes(displayToken, new int[]{
-                HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
-                HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
- 
+        RefreshUtils.startService(context); 
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
         FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0"); 
     }
