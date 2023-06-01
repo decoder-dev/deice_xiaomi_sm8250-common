e09b502817945ac712eee8f667f7c8ae2d3d82df
EndCredits <endcredits@crepuscular-aosp.icu>
2023-06-01 19:30:15 +0530

    sm8250-common: Stop building libdisplayconfig.qti vendor variant

--- kona.mk
+++ kona.mk
@@ -260,7 +260,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
