8bb21c469c46f0d12fa04f125bba724fe7a43349
decoder <toniron208@gmail.com>
2023-06-25 13:48:14 +0300

    update

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,7 +133,9 @@ KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 LLVM_IAS=1
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
-TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument" \
+    TARGET_PRODUCT=$(PRODUCT_DEVICE)
 
 
 
